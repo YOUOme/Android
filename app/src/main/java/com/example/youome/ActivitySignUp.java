6b20690e1145e606c80c619ff516f68c3f4d7cac
donghan <tnqkr98@naver.com>
2020-06-03 23:33:25 +0900

    Update SignUpActivity

--- app/src/main/java/com/example/youome/ActivitySignUp.java
+++ app/src/main/java/com/example/youome/ActivitySignUp.java
@@ -1,29 +1,34 @@
 package com.example.youome;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.KeyEvent;
 import android.view.MotionEvent;
 import android.view.View;
-import android.widget.EditText;
-import android.widget.LinearLayout;
 import android.widget.TextView;
 
-import com.google.android.material.bottomsheet.BottomSheetDialogFragment;
 import com.google.android.material.textfield.TextInputEditText;
 
 public class ActivitySignUp extends AppCompatActivity {
 
-    TextInputEditText et_phone;
-    LinearLayout linearLayout_Auth;
+    TextInputEditText et_phone,et_name,et_id;
     boolean onetime = true;
+
+    FragmentSignUp1Auth auth;
+    FragmentSignUp2Tele tele;
+
+    FragmentTransaction ft;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
         et_phone = (TextInputEditText)findViewById(R.id.et_phone);
+        et_phone.setOnTouchListener(new SignUpClickListener());
         et_phone.setOnEditorActionListener(new TextView.OnEditorActionListener() {
             @Override
             public boolean onEditorAction(TextView textView, int i, KeyEvent keyEvent) {
@@ -32,18 +37,39 @@ public class ActivitySignUp extends AppCompatActivity {
                 return false;
             }
         });
+        et_name = (TextInputEditText)findViewById(R.id.et_name);
+        et_name.setOnTouchListener(new SignUpClickListener());
+        et_id = (TextInputEditText)findViewById(R.id.et_inum);
+        et_id.setOnTouchListener(new SignUpClickListener());
 
-        linearLayout_Auth = (LinearLayout)findViewById(R.id.auth);
-        linearLayout_Auth.setOnTouchListener(new View.OnTouchListener() { //  한 리스너에 때려박자.
-            @Override
-            public boolean onTouch(View view, MotionEvent motionEvent) {
-                if(onetime) {
-                    onetime = false;
-                    FragmentSignUpAuth bot = new FragmentSignUpAuth();
-                    bot.show(getSupportFragmentManager(), bot.getTag());
-                }
-                return false;
+        //auth = new FragmentSignUp1Auth();
+        tele = new FragmentSignUp2Tele();
+        //tele.setCancelable(false);
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        //ft = getSupportFragmentManager().beginTransaction();
+    }
+
+    private class SignUpClickListener implements View.OnTouchListener{
+        @Override
+        public boolean onTouch(View view, MotionEvent motionEvent) {
+            if(onetime) {
+                //auth.show(getSupportFragmentManager(), auth.getTag());
+                //ft.commit();
+                //ft.add(auth,"auth");
+                //ft.commit();
+                FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+                fragmentTransaction.add(auth,"auth");
+                fragmentTransaction.commit();
+                onetime = false;
             }
-        });
+            if(!onetime && view.getId() == R.id.et_phone){
+                //ft.commit();
+                //FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+                //fragmentTransaction.add(tele,"tele").commit();
+            }
+            return false;
+        }
     }
 }
---
+++ app/src/main/java/com/example/youome/FragmentSignUp2Tele.java
@@ -0,0 +1,45 @@
+package com.example.youome;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.FragmentManager;
+
+public class FragmentSignUp2Tele extends BottomSheetDialogFragment {
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setStyle(BottomSheetDialogFragment.STYLE_NORMAL, R.style.CustomBottomSheetDialogTheme);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        final View view = inflater.inflate(R.layout.fragment_signuptele, container, false);
+
+
+        return view;
+    }
+
+    @Override
+    public void show(FragmentManager manager, String tag) {
+        try {
+            // Add a remove transaction before each add transaction to prevent continuous add
+            manager.beginTransaction().remove(this).commit();
+            super.show(manager, tag);
+        } catch (Exception e) {
+            // The same instance will use different tags will be an exception, capture here
+            e.printStackTrace();
+        }
+    }
+
+}
